cdd496cb19521bd7bd30cd95bfcd2b3ecbe8950d
Stefan Willoughby <svonwolf@gmail.com>
2014-04-03 16:10:39 +1100

    Ep. 3
    Unity3d Tutorial - Enemy AI 1/2

---
+++ Assets/01.scripts/EnemyAI.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyAI : MonoBehaviour {
+
+	public Transform target;
+	public int moveSpeed;
+	public int rotationSpeed;
+	public int maxDistance;
+
+	private Transform myTransform;
+
+	void Awake() {
+		myTransform = transform;
+	}
+
+	// Use this for initialization
+	void Start () {
+		GameObject go = GameObject.FindGameObjectWithTag("Player");
+		
+		target = go.transform;
+		
+		maxDistance = 2;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Debug.DrawLine(target.position, myTransform.position, Color.yellow);
+
+		// Look at target
+		myTransform.rotation = Quaternion.Slerp
+	}
+}
