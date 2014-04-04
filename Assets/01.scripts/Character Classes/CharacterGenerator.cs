ba995bf21fa6c1fa6afb5cfb74cb947796264b24
Stefan Willoughby <svonwolf@gmail.com>
2014-04-04 13:16:54 +1100

    Ep. 21

---
+++ Assets/01.scripts/Character Classes/CharacterGenerator.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterGenerator : MonoBehaviour {
+
+	private PlayerCharacter _toon;
+
+	// Use this for initialization
+	void Start () {
+		_toon = new PlayerCharacter();
+		_toon.Awake();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnGUI () {
+//		GUI.skin = mySkin;
+		
+		DisplayName();
+//		DisplayPointsLeft();
+//		DisplayAttributes();
+//		DisplayVitals();
+//		DisplaySkills();
+		
+		
+	}
+	private void DisplayName() {
+		GUI.Label(new Rect(10, 10, 50, 25), "Name:");
+		_toon.Name = GUI.TextField(new Rect(115, 10, 100, 25), _toon.Name);
+	}
+
+}
